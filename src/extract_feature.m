d84863b11e77fc5ef735651f1207cb456ae97483
Nuullll <vfirst218@gmail.com>
2018-04-05 18:46:41 +0800

    Restructure.

--- src/extract_feature.m
+++ src/extract_feature.m
@@ -1,28 +1,9 @@
 function [ feature ] = extract_feature( img )
 % Feature extractor
 
-% divide img into block_sizexblock_size blocks and count black pixels in each block
-% accept binarized image
-% resize template image `img` to norm_sizexnorm_size
-% therefore `feature` is a row vector
+extract_fun = @blockwise_counter;
 
-norm_size = 32;
-block_size = 2;
-step = 2;
-count = norm_size/step - block_size/step + 1;
-
-res_img = imbinarize(imresize(double(~img),[norm_size norm_size]));
-
-blocks = ones(block_size,block_size,count,count);
-
-for i = 1:count
-    for j = 1:count
-        blocks(:,:,i,j) = res_img((i-1)*step+1:(i-1)*step+block_size,...
-            (j-1)*step+1:(j-1)*step+block_size);
-    end
-end
-
-feature = reshape(squeeze(sum(sum(blocks))),1,[]);
+feature = extract_fun(img);
 
 end
 
